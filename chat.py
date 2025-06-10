7fc63f09a19859917dbf4a856c27536e3c746d07
lifeiscabaret <lifeiscabaret1998@gmail.com>
2025-06-10 13:52:50 +0900

    refactor: modularize chatbot logic into separate llm.py file

--- chat.py
+++ chat.py
@@ -1,13 +1,5 @@
 import streamlit as st
-import os
-
-from dotenv import load_dotenv
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain_pinecone import PineconeVectorStore
-from pinecone import Pinecone
-from langchain import hub
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
+from llm import get_ai_message
 
 
 st.set_page_config(page_title='전세사기피해 상담 챗봇', page_icon='🤖')
@@ -16,79 +8,27 @@ st.title('🤖 전세사기피해 상담 챗봇')
 if 'message_list' not in st.session_state:
     st.session_state.message_list = []
 
-print(f'before: {st.session_state.message_list}')
 
-## 이전 채팅 내용 화면 출력
-# for message in st.session_state.message_list:
-#     print('message>> ', message)
-#     print('message["role"]>> ', message['role'])
-#     print('message["content"] >>', message['content'])
-
-# 지피티 방법
+## 이전 채킹 내용 화면 출력
 for msg in st.session_state.message_list:
     with st.chat_message(msg['role']):
         st.write(msg['content'])
 
 
-## [AI Message 함수 정의] ########################################################
-def get_ai_message(user_message):
-    
-
-    ## 환경변수 읽어오기  ################################################################################
-    load_dotenv()
-    PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
-    LANGCHAIN_API_KEY = os.getenv('LANGCHAIN_API_KEY')
-
-
-    ## 벡터 스토어(데이터베이스)에서 인덱스 가져오기 ##################################################################
-    ## 임베딩 모델 지정
-    embedding = OpenAIEmbeddings(model='text-embedding-3-large')
-    pc = Pinecone(api_key=PINECONE_API_KEY)
-    index_name = 'law' #변수에 저장
-
-
-
-
-    # ## 저장된 인덱스 가져오기
-    database = PineconeVectorStore.from_existing_index(
-        index_name=index_name,
-        embedding=embedding,
-    )
-
-
-
-    ## RetrievalQA ##################################################################################
-    llm = ChatOpenAI(model='gpt-4o')
-    prompt = hub.pull('rlm/rag-prompt')
-
-    def format_docs(docs):
-        return'\n\n'.join(doc.page_content for doc in docs)
-
-    qa_chain = ( #구성(설계)
-        {
-            'context' : database.as_retriever() | format_docs,
-            'question' : RunnablePassthrough(),
-
-        }
-        | prompt
-        | llm
-        | StrOutputParser()
-    )
-
-    # qa_chain.invoke('전세사기피해자 대상을 알려주세요.')
-    ai_message = qa_chain.invoke(user_message)
-    return ai_message
-
-
-## prompt 창 #####################################################################
+## 채팅 메시지 =======================================================================
 placeholder = '전세사기피해와 관련된 궁금한 내용을 질문하세요.'
-if user_question := st.chat_input(placeholder=placeholder):
+if user_question := st.chat_input(placeholder=placeholder): ## prompt 창
+    ## 사용자 메시지 ######################################
     with st.chat_message('user'):
         ## 사용자 메시지 화면 출력
         st.write(user_question)
     st.session_state.message_list.append({'role': 'user', 'content': user_question})
 
-    ai_message = get_ai_message(user_question)
+
+    # AI 메시지 ###########################################
+    # spinner 추가
+    with st.spinner('잠시만 기다려주세요:)'):
+        ai_message = get_ai_message(user_question)
 
     with st.chat_message('ai'):
         ## AI 메시지 화면 출력
@@ -97,14 +37,7 @@ if user_question := st.chat_input(placeholder=placeholder):
 
 print(f'after: {st.session_state.message_list}') 
 
-print(user_question)
 
-## ============================
-# st.chat_message('user')
-# st.write('안녕하세요!')
-
-
-# st.chat_message('ai').write('안녕~')
 
 
 
--- llm.py
+++ llm.py
@@ -8,38 +8,45 @@ from langchain import hub
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 
+## 환경변수 읽어오기 ==============================================================
+load_dotenv() 
 
 
-## [AI Message 함수 정의] ########################################################
-def get_ai_message(user_message):
-    
+## llm 함수 정의 ===================================================================
+def get_llm(model='gpt-4o'):
+    llm = ChatOpenAI(model=model) 
+    return llm
 
-    ## 환경변수 읽어오기  ################################################################################
-    load_dotenv()
+## database 함수 정의 ================================================
+def get_database():
     PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
-    LANGCHAIN_API_KEY = os.getenv('LANGCHAIN_API_KEY')
 
-
-    ## 벡터 스토어(데이터베이스)에서 인덱스 가져오기 ##################################################################
     ## 임베딩 모델 지정
     embedding = OpenAIEmbeddings(model='text-embedding-3-large')
-    pc = Pinecone(api_key=PINECONE_API_KEY)
+    Pinecone(api_key=PINECONE_API_KEY)
     index_name = 'law' #변수에 저장
 
-
-
-
-    # ## 저장된 인덱스 가져오기
+    ## 저장된 인덱스 가져오기
     database = PineconeVectorStore.from_existing_index(
         index_name=index_name,
         embedding=embedding,
     )
 
+    return database
 
 
-    ## RetrievalQA ##################################################################################
-    llm = ChatOpenAI(model='gpt-4o')
-    prompt = hub.pull('rlm/rag-prompt')
+## retrievalQA 함수 정의 ====================================================================
+def get_retrievalQA():
+    LANGCHAIN_API_KEY = os.getenv('LANGCHAIN_API_KEY')
+
+    ## vector store에서 index 정보
+    database = get_database()
+
+    ## prompt
+    prompt = hub.pull('rlm/rag-prompt', api_key=LANGCHAIN_API_KEY)
+
+    ## LLM 모델 지정
+    llm = get_llm()
 
     def format_docs(docs):
         return'\n\n'.join(doc.page_content for doc in docs)
@@ -55,6 +62,15 @@ def get_ai_message(user_message):
         | StrOutputParser()
     )
 
-    # qa_chain.invoke('전세사기피해자 대상을 알려주세요.')
+    return qa_chain
+
+
+
+
+
+## [AI Message 함수 정의] -------------------------------------------------------------
+def get_ai_message(user_message):
+    qa_chain = get_retrievalQA()
     ai_message = qa_chain.invoke(user_message)
+
     return ai_message
