82612d3ed1a26af90333e435f9760fb28eb3ba79
lifeiscabaret <lifeiscabaret1998@gmail.com>
2025-06-11 17:14:11 +0900

    feat: enhance chatbot with contextual memory per user session

--- chat.py
+++ chat.py
@@ -2,43 +2,37 @@ import streamlit as st
 from llm import get_ai_message
 
 
-st.set_page_config(page_title='전세사기피해 상담 챗봇', page_icon='🤖')
-st.title('🤖 전세사기피해 상담 챗봇')
+st.set_page_config(page_title='전세사기피해 상담 챗봇', page_icon='🐾')
+st.title('🐾 전세사기피해 상담 챗봇')
 
 if 'message_list' not in st.session_state:
     st.session_state.message_list = []
 
+## 이전 채팅 내용 화면 출력
+for message in st.session_state.message_list:
+    with st.chat_message(message['role']):
+        st.write(message['content'])
 
-## 이전 채킹 내용 화면 출력
-for msg in st.session_state.message_list:
-    with st.chat_message(msg['role']):
-        st.write(msg['content'])
 
-
-## 채팅 메시지 =======================================================================
+## 채팅 메시지 ======================================================================
 placeholder = '전세사기피해와 관련된 궁금한 내용을 질문하세요.'
 if user_question := st.chat_input(placeholder=placeholder): ## prompt 창
-    ## 사용자 메시지 ######################################
+    ## 사용자 메시지 ##############################
     with st.chat_message('user'):
         ## 사용자 메시지 화면 출력
         st.write(user_question)
     st.session_state.message_list.append({'role': 'user', 'content': user_question})
 
+    ## AI 메시지 ##################################
+    with st.spinner('답변 생성하는 중입니다.'):
+        # ai_message = get_ai_message(user_question)
 
-    # AI 메시지 ###########################################
-    # spinner 추가
-    with st.spinner('잠시만 기다려주세요:)'):
-        ai_message = get_ai_message(user_question)
+        session_id = 'user-session'
+        ai_message = get_ai_message(user_question, session_id=session_id)
 
-    with st.chat_message('ai'):
-        ## AI 메시지 화면 출력
-        st.write(ai_message)
-    st.session_state.message_list.append({'role': 'ai', 'content': ai_message})
+        with st.chat_message('ai'):
+            ## AI 메시지 화면 출력
+            st.write(ai_message)
+        st.session_state.message_list.append({'role': 'ai', 'content': ai_message})
 
-print(f'after: {st.session_state.message_list}') 
-
-
-
-
-
-
+print(f'after: {st.session_state.message_list}')
--- llm.py
+++ llm.py
@@ -1,78 +1,122 @@
 import os
 
 from dotenv import load_dotenv
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables import RunnableLambda
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
 from pinecone import Pinecone
-from langchain import hub
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
-
-## 환경변수 읽어오기 ==============================================================
-load_dotenv() 
 
 
-## llm 함수 정의 ===================================================================
+## 환경변수 읽어오기 =====================================================
+load_dotenv()
+
+## llm 함수 정의 =========================================================
 def get_llm(model='gpt-4o'):
-    return ChatOpenAI(model=model)
+    llm = ChatOpenAI(model=model)
+    return llm
 
-print("OPENAI_API_KEY 불러오기:", os.getenv("OPENAI_API_KEY"))
-
-## database 함수 정의 ================================================
+## database 함수 정의 ======================================================
 def get_database():
     PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
 
-    ## 임베딩 모델 지정text-embedding-3-large')
-    embedding = OpenAIEmbeddings(
-        model="text-embedding-3-large",
-    )
-
+    ## 임베딩 모델 지정
+    embedding = OpenAIEmbeddings(model='text-embedding-3-large')
     Pinecone(api_key=PINECONE_API_KEY)
-    index_name = 'law' #변수에 저장
+    index_name = 'law'
 
     ## 저장된 인덱스 가져오기
-    return PineconeVectorStore.from_existing_index(
+    database = PineconeVectorStore.from_existing_index(
         index_name=index_name,
-        embedding=embedding
+        embedding=embedding,
     )
 
-## retrievalQA 함수 정의 ====================================================================
+    return database
+
+
+## Statefully manage chat history ========================================
+store = {}
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
+
+## retrievalQA 함수 정의 =================================================
 def get_retrievalQA():
     LANGCHAIN_API_KEY = os.getenv('LANGCHAIN_API_KEY')
 
     ## vector store에서 index 정보
     database = get_database()
 
-    ## prompt
-    # prompt = hub.pull('rlm/rag-prompt'), api_key=LANGCHAIN_API_KEY)
-    prompt = hub.pull('lifeiscabaret/rag-prompt', api_key=LANGCHAIN_API_KEY)
+
+    ### Answer question ###
+    system_prompt = (
+    '''[identity]
+- 당신은 전세사기피해 법률 전문가입니다.
+- [context]를 참고하여 사용자의 질문에 답변하세요.
+- 답변에는 해당 조항을 '(XX법 제X조 제X항 제X호, XX법 제X조 제X항 제X호)' 형식으로 문단 마지막에 표시하세요.
+- 항목별로 표시해서 답변해주세요.
+- 전세사기피해 법률 이외의 질문에는 '답변할 수 없습니다.'로 답하세요.
+
+[context]
+{context} 
+'''    
+    )
+
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
 
     ## LLM 모델 지정
     llm = get_llm()
 
     def format_docs(docs):
-        return'\n\n'.join(doc.page_content for doc in docs)
+        return '\n\n'.join(doc.page_content for doc in docs)
+    
+    input_str = RunnableLambda(lambda x: x['input'])
 
-    qa_chain = ( #구성(설계)
+    qa_chain = (
         {
-            'context' : database.as_retriever() | format_docs,
-            'question' : RunnablePassthrough(),
-
+            'context': input_str | database.as_retriever() | format_docs,
+            'input': input_str,
+            'chat_history': RunnableLambda(lambda x: x['chat_history'])
         }
-        | prompt
+        | qa_prompt
         | llm
         | StrOutputParser()
     )
 
-    return qa_chain
+    conversational_rag_chain = RunnableWithMessageHistory(
+        qa_chain,
+        get_session_history,
+        input_messages_key="input",
+        history_messages_key="chat_history",
+    )
+
+    return conversational_rag_chain
 
 
-
-
-
-## [AI Message 함수 정의] -------------------------------------------------------------
-def get_ai_message(user_message):
+## [AI Message 함수 정의] ================================================
+def get_ai_message(user_message, session_id='default'):
     qa_chain = get_retrievalQA()
-    ai_message = qa_chain.invoke(user_message)
+
+    ai_message = qa_chain.invoke(
+        {'input': user_message},
+        config={'configurable': {'session_id': session_id}},        
+    )
+
+    # print(f'대화 이력 >> {get_session_history(session_id)} \n😊\n')
+    # print('=' * 50 + '\n')
 
     return ai_message
+
